159b387d6ccef87f043e4964dc102576e637d353
Filipe Scaglia <filipecscaglia@gmail.com>
2021-02-13 16:50:03 -0300

    Custom ConstraintValidator

--- backend/src/main/java/com/filipescaglia/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/filipescaglia/dscatalog/dto/UserInsertDTO.java
@@ -1,5 +1,8 @@
 package com.filipescaglia.dscatalog.dto;
 
+import com.filipescaglia.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 	
--- backend/src/main/java/com/filipescaglia/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/filipescaglia/dscatalog/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import com.filipescaglia.dscatalog.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	
+	User findByEmail(String email);
 
 }
