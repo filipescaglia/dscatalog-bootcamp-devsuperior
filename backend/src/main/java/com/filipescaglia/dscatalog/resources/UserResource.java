ab24b6036c857e963ebdfe35cda8af4bf833ebf1
Filipe Scaglia <filipecscaglia@gmail.com>
2021-02-13 17:33:47 -0300

    UserUpdateValidator

--- backend/src/main/java/com/filipescaglia/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/filipescaglia/dscatalog/resources/UserResource.java
@@ -22,6 +22,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.filipescaglia.dscatalog.dto.UserDTO;
 import com.filipescaglia.dscatalog.dto.UserInsertDTO;
+import com.filipescaglia.dscatalog.dto.UserUpdateDTO;
 import com.filipescaglia.dscatalog.services.UserService;
 
 @RestController
@@ -60,9 +61,9 @@ public class UserResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserDTO dto) {
-		dto = service.update(id, dto);
-		return ResponseEntity.ok().body(dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id, @Valid @RequestBody UserUpdateDTO dto) {
+		UserDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	}
 	
 	@DeleteMapping(value = "/{id}")
